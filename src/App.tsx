487a5b090507fb3b08293e9047f6ac2fe64fa923
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-07-22 15:38:21 -0300

    feat: create sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
+// eslint-disable-next-line import/order
 import { RouterProvider } from 'react-router-dom';
+
+import './global.css';
+
+import { Toaster } from 'sonner';
 
 import { router } from './routes';
 
@@ -8,6 +13,8 @@ export function App() {
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
       <RouterProvider router={router} />
+
+      <Toaster richColors />
     </HelmetProvider>
   );
 }
