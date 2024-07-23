54fa5b477b5158640c91ec2256be3ff030c28011
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-07-23 15:24:30 -0300

    feat: create pagination component

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 
+import { Pagination } from '@/components/pagination';
 import {
   Table,
   TableBody,
@@ -17,30 +18,32 @@ export function Orders() {
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
 
-      <div className="space-y-2.5">
-        <OrderTableFilters />
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[64px]" />
-                <TableHead className="w-[220px]">Identificador</TableHead>
-                <TableHead className="w-[180px]">Realizado há</TableHead>
-                <TableHead className="w-[140px]">Status</TableHead>
-                <TableHead>Cliente</TableHead>
-                <TableHead className="w-[140px]">Total do pedidos</TableHead>
-                <TableHead className="w-[164px]" />
-                <TableHead className="w-[132px]" />
-              </TableRow>
-            </TableHeader>
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]" />
+                  <TableHead className="w-[220px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedidos</TableHead>
+                  <TableHead className="w-[164px]" />
+                  <TableHead className="w-[132px]" />
+                </TableRow>
+              </TableHeader>
 
-            <TableBody>
-              <OrderTableRow />
-            </TableBody>
-          </Table>
+              <TableBody>
+                <OrderTableRow />
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} perPage={10} totalCount={105} />
         </div>
       </div>
     </>
