182d26157fb0ddf9397b280fb356dcfc2160f4b6
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-08-13 11:20:48 -0300

    chore: setup test environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@
 import path from 'node:path';
 
 import react from '@vitejs/plugin-react-swc';
+import type { UserConfig } from 'vite';
 import { defineConfig } from 'vite';
+import type { InlineConfig } from 'vitest/node';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,4 +14,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig;
 });
