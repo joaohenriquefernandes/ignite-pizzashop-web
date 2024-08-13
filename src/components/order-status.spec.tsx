5962ea1e0a0dbab0de20f4df6acd1e55f638a582
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-08-13 11:44:02 -0300

    test: create unit test order status

--- src/components/order-status.spec.tsx
+++ src/components/order-status.spec.tsx
@@ -1,3 +1,50 @@
-test('1 + 1 equals 2', () => {
-  expect(1 + 1).toEqual(2);
+import { render } from '@testing-library/react';
+
+import { OrderStatus } from './order-status';
+
+describe('Order Status', () => {
+  it('should display the right text when order status is pending', () => {
+    const wrapper = render(<OrderStatus status="pending" />);
+
+    wrapper.debug();
+
+    expect(wrapper.getByText('Pendente')).toBeInTheDocument();
+    expect(wrapper.getByTestId('badge')).toHaveClass('bg-slate-400');
+  });
+
+  it('should display the right text when order status is canceled', () => {
+    const wrapper = render(<OrderStatus status="canceled" />);
+
+    wrapper.debug();
+
+    expect(wrapper.getByText('Cancelado')).toBeInTheDocument();
+    expect(wrapper.getByTestId('badge')).toHaveClass('bg-rose-500');
+  });
+
+  it('should display the right text when order status is delivering', () => {
+    const wrapper = render(<OrderStatus status="delivering" />);
+
+    wrapper.debug();
+
+    expect(wrapper.getByText('Em entrega')).toBeInTheDocument();
+    expect(wrapper.getByTestId('badge')).toHaveClass('bg-amber-500');
+  });
+
+  it('should display the right text when order status is delivered', () => {
+    const wrapper = render(<OrderStatus status="delivered" />);
+
+    wrapper.debug();
+
+    expect(wrapper.getByText('Entregue')).toBeInTheDocument();
+    expect(wrapper.getByTestId('badge')).toHaveClass('bg-emerald-500');
+  });
+
+  it('should display the right text when order status is delivered', () => {
+    const wrapper = render(<OrderStatus status="processing" />);
+
+    wrapper.debug();
+
+    expect(wrapper.getByText('Em preparo')).toBeInTheDocument();
+    expect(wrapper.getByTestId('badge')).toHaveClass('bg-amber-500');
+  });
 });
--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -21,19 +21,31 @@ export function OrderStatus({ status }: IOrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       {status === 'canceled' && (
-        <span className="h-3 w-3 rounded-full bg-rose-500" />
+        <span
+          data-testid="badge"
+          className="h-3 w-3 rounded-full bg-rose-500"
+        />
       )}
 
       {status === 'pending' && (
-        <span className="h-3 w-3 rounded-full bg-slate-400" />
+        <span
+          data-testid="badge"
+          className="h-3 w-3 rounded-full bg-slate-400"
+        />
       )}
 
       {status === 'delivered' && (
-        <span className="h-3 w-3 rounded-full bg-emerald-500" />
+        <span
+          data-testid="badge"
+          className="h-3 w-3 rounded-full bg-emerald-500"
+        />
       )}
 
       {['processing', 'delivering'].includes(status) && (
-        <span className="h-3 w-3 rounded-full bg-amber-500" />
+        <span
+          data-testid="badge"
+          className="h-3 w-3 rounded-full bg-amber-500"
+        />
       )}
 
       <span className="font-medium text-muted-foreground">
